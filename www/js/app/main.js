22fc8242c98217e3aea3425004c5aa4a62ce154f
Dang Mai <contact@dangmai.net>
2012-09-25 00:33:33 -0400

    Major code style overhaul to conform with JSLint
    Change a lot of code style - most importantly, moving all vars to the
    beginning of a function.
    Change "people" to "person" in necessary context(s).

--- www/js/app/main.js
+++ www/js/app/main.js
@@ -1,125 +1,143 @@
+/*global define, moment, console*/
 (function () {
 	'use strict';
 
-	define(function (require) {
-		var Mustache = require('mustache'),
-			formTemplateSrc = require('text!app/templates/form.html'),
-			billRowSrc = require('text!app/templates/bill-row.html'),
-			peopleRowSrc = require('text!app/templates/person-row.html'),
-			resultSrc = require('text!app/templates/result.html'),
-			$ = require('jquery'),
-			split = require('app/split');
-		require("jquery.validate");
-		require("domReady!");
-		require("bootstrap-datepicker");
-		
-		var	peopleCount = 0,
-			billCount = 0,
-			removable = function(count) {
-				return !(count === 1);
-			},
-			formTmpl = Mustache.render(formTemplateSrc);
-			
-		$('.main-form-wrapper').html(formTmpl);
+	define(['app/split', 'mustache', 'text!app/templates/form.html',
+		'text!app/templates/bill-row.html',
+		'text!app/templates/person-row.html', 'text!app/templates/result.html',
+		'jquery', 'jquery.validate', 'domReady!', 'bootstrap-datepicker'],
+		function (split, Mustache, formTemplateSrc, billRowSrc, personRowSrc,
+			resultSrc, $) {
 
-		/**
-		 * Start the calculations!
-		 */
-		var calculate = function() {
-			var people = [];
-			var bills = [];
-			$.each($('#main-form tr[id^=person]'), function(index, el) {
-				var name = $(el).find('[name^=name]').val(),
-					inDate = $(el).find('[name^=in]').val(),
-					outDate = $(el).find('[name^=out]').val();
-				people.push(split.personify({"name": name, "in": inDate, "out": outDate}));
-			});
-			$.each($('#main-form tr[id^=bill]'), function(index, el) {
-				var forVal = $(el).find('[name^=for]').val(),
-					start = $(el).find('[name^=start]').val(),
-					end = $(el).find('[name^=end]').val(),
-					total = parseFloat($(el).find('[name^=total]').val());
-				bills.push(split.billify({"for": forVal, "start": start, "end": end, "total": total}));
-			});
-			var results = split.calculate(bills, people);
-			$('.result').html(Mustache.render(resultSrc, { "people": results }));
-		};
+			var	peopleCount = 0,
+				billCount = 0,
+				formTmpl = Mustache.render(formTemplateSrc),
+				removable,
+				calculate,
+				initializeDatepicker;
 
-		// Validationa on the form
-		$.validator.addMethod("moment", function(value, element) {
-			return this.optional(element) || split.validateDate(value);
-		}, "Not a date");
-		$.validator.addMethod("laterthan", function(value, element, startEl) {
-			return this.optional(element) || this.optional($(startEl).get(0)) || (moment(value).diff(moment($(startEl).val())) > 0);
-		}, "Sooner than start");
-		$.validator.messages.required = $.format("Required");
-		$.validator.messages.number = $.format("Not a number");
-		var validator = $('#main-form').validate({
-			debug: true,
-			submitHandler: function(form) {
-				//console.log("submithandler");
-				calculate();
-			},
-			invalidHandler: function() {},
-			highlight: function(label) {
-				$(label).closest('.control-group').addClass('error');
-			},
-			unhighlight: function(label) {
-				$(label).closest('.control-group').removeClass('error');
-			},
-		});
+			removable = function (count) {
+				return (count !== 1);
+			};
+			$('.main-form-wrapper').html(formTmpl);
 
-		$('form').on("click", ".remove", {}, function(e) {
-			e.preventDefault();
-			$(this).parent().parent().remove();
-		});
+			/**
+			 * Start the calculations!
+			 */
+			calculate = function () {
+				var people = [],
+					bills = [],
+					results;
+				$.each($('#main-form tr[id^=person]'), function (index, el) {
+					var name = $(el).find('[name^=name]').val(),
+						inDate = $(el).find('[name^=in]').val(),
+						outDate = $(el).find('[name^=out]').val();
+					people.push(split.personify({"name": name, "in": inDate, "out": outDate}));
+				});
+				$.each($('#main-form tr[id^=bill]'), function (index, el) {
+					var forVal = $(el).find('[name^=for]').val(),
+						start = $(el).find('[name^=start]').val(),
+						end = $(el).find('[name^=end]').val(),
+						total = parseFloat($(el).find('[name^=total]').val());
+					bills.push(split.billify({"for": forVal, "start": start, "end": end, "total": total}));
+				});
+				results = split.calculate(bills, people);
+				$('.result').html(Mustache.render(resultSrc, { "people": results }));
+			};
 
-		/**
-		 * Initialize bootstrap-datepicker on a certain element. This will 
-		 * check for all alements with class date-field inside the input element
-		 * and initialize datepicker on them.
-		 * @param (HTMLElement) el the element to initialize datepicker on.
-		 */
-		var initializeDatepicker = function(el) {
-			var dateFields = $(el).find(".date-field");
-			dateFields.datepicker({
-				format: 'M dd, yyyy'
+			// Validationa on the form
+			$.validator.addMethod("moment", function (value, element) {
+				return this.optional(element) || split.validateDate(value);
+			}, "Not a date");
+			$.validator.addMethod("laterthan", function (value, element, startEl) {
+				return this.optional(element) || this.optional($(startEl).get(0)) || (moment(value).diff(moment($(startEl).val())) > 0);
+			}, "Sooner than start");
+			$.validator.messages.required = $.format("Required");
+			$.validator.messages.number = $.format("Not a number");
+			$('#main-form').validate({
+				debug: true,
+				submitHandler: function (form) {
+					//console.log("submithandler");
+					calculate();
+				},
+				invalidHandler: function () {},
+				highlight: function (label) {
+					$(label).closest('.control-group').addClass('error');
+				},
+				unhighlight: function (label) {
+					$(label).closest('.control-group').removeClass('error');
+				}
 			});
-			dateFields.datepicker().on('changeDate', function(e) {
-				$('#main-form').valid();
+
+			$('form').on("click", ".remove", {}, function (e) {
+				e.preventDefault();
+				$(this).parent().parent().remove();
 			});
-		};
-		$('#more-people').click(function(e, context) {
-			e.preventDefault();
-			if (!context) context = {};
-			peopleCount ++;
-			var defContext = { peopleId: peopleCount, removable: removable(peopleCount) };
-			$.extend(context, defContext);
-			var tmpl = Mustache.render(peopleRowSrc, context);
-			initializeDatepicker($(tmpl).appendTo($('#people-table')));		
+
+			/**
+			 * Initialize bootstrap-datepicker on a certain element. This will 
+			 * check for all alements with class date-field inside the input element
+			 * and initialize datepicker on them.
+			 * @param (HTMLElement) el the element to initialize datepicker on.
+			 */
+			initializeDatepicker = function (el) {
+				var dateFields = $(el).find(".date-field");
+				dateFields.datepicker({
+					format: 'M dd, yyyy'
+				});
+				dateFields.datepicker().on('changeDate', function (e) {
+					$('#main-form').valid();
+				});
+			};
+			$('#more-people').click(function (e, context) {
+				e.preventDefault();
+				var defContext,
+					tmpl;
+				if (!context) {
+					context = {};
+				}
+				peopleCount += 1;
+				defContext = { peopleId: peopleCount, removable: removable(peopleCount) };
+				$.extend(context, defContext);
+				tmpl = Mustache.render(personRowSrc, context);
+				initializeDatepicker($(tmpl).appendTo($('#people-table')));
+			});
+			$('#more-bills').click(function (e, context) {
+				e.preventDefault();
+				var defContext,
+					tmpl;
+				if (!context) {
+					context = {};
+				}
+				billCount += 1;
+				defContext = { billId : billCount, removable: removable(billCount) };
+				$.extend(context, defContext);
+				tmpl = Mustache.render(billRowSrc, context);
+				initializeDatepicker($(tmpl).appendTo($('#bills-table')));
+			});
+
+			$('#more-people').trigger("click", {
+				"name": "Dang Mai",
+				"in": "Aug 05, 2012",
+				"out": "Sept 05, 2012"
+			});
+			$('#more-people').trigger("click", {
+				"name": "Saqib",
+				"in": "Aug 05, 2012",
+				"out": "Sept 05, 2012"
+			});
+
+			$('#more-bills').trigger("click", {
+				"for": "Electricity",
+				"total": 30,
+				"start": "Aug 06, 2012",
+				"end": "Sept 03, 2012"
+			});
+			$('#more-bills').trigger("click", {
+				"for": "Internet",
+				"total": 60,
+				"start": "Aug 06, 2012",
+				"end": "Sept 03, 2012"
+			});
 		});
-		$('#more-bills').click(function(e, context) {
-			e.preventDefault();
-			if (!context) context = {};
-			billCount ++;
-			var defContext = { billId : billCount, removable: removable(billCount) };
-			$.extend(context, defContext);
-			var tmpl = Mustache.render(billRowSrc, context)
-			initializeDatepicker($(tmpl).appendTo($('#bills-table')));
-		});
-		
-		
-		$('#more-people').trigger("click", {"name": "Dang Mai", "in": "Aug 05, 2012", "out": "Sept 05, 2012"});
-		$('#more-people').trigger("click",{"name": "Saqib", "in": "Aug 05, 2012", "out": "Sept 05, 2012"});
-		
-		//$('#more-people').trigger("click");
-		//$('#more-people').trigger("click");
-		
-		$('#more-bills').trigger("click",{"for":"Electricity", "total": 30, "start": "Aug 06, 2012", "end": "Sept 03, 2012"});
-		$('#more-bills').trigger("click",{"for":"Internet", "total": 60, "start": "Aug 06, 2012", "end": "Sept 03, 2012"});
-		//$('#more-bills').trigger("click");
-		//$('#more-bills').trigger("click");
-		//$('#more-bills').trigger("click",{});
-		
-	});
 }());
--- www/js/app/split.js
+++ www/js/app/split.js
@@ -1,51 +1,18 @@
+/*global define,$M,$V*/
 (function () {
 	'use strict';
 
-	define(['underscore', 'jquery', 'moment-range', 'sylvester'], function (_, $, moment, sylvester){
-		var dateFormat = "MMM DD, YYYY";
+	define(['app/day', 'underscore', 'jquery', 'moment-range', 'sylvester'], function (Day, _, $, moment, sylvester) {
+		var dateFormat = "MMM DD, YYYY",
+			calculate,
+			billReviver,
+			billify,
+			billReplacer,
+			peopleReviver,
+			peopleReplacer,
+			personify,
+			validateDate;
 
-		/**
-		 * A class to represent a day in the billing cycle of a particular bill.
-		 * @param (moment) date the date of this day.
-		 * @param (int) billIndex the index of this bill in the bills array.
-		 */
-		function Day(date, billIndex) {
-			this.date = date;
-			this.billIndex = billIndex;
-			this.people = [];
-		}
-		Day.prototype.toString = function() {
-			return this.date.toString();
-		}
-		/**
-		 * Add a person who is present on this day.
-		 * @param (object) person the person object.
-		 */
-		Day.prototype.addPerson = function(person) {
-			this.people.push(person);
-		}
-		/**
-		 * Announces to this object how much it is worth in the billing
-		 * cycle. Calling this automatically distribute this amount of
-		 * money evenly towards the people who are present on this day.
-		 * @param moneyAmt how much this day is worth in the billing cycle.
-		 */
-		Day.prototype.worths = function(moneyAmt) {
-			var eachPersonOwes = moneyAmt / this.people.length;
-			var that = this;
-			
-			$.each(this.people, function(index, person) {
-				person['owes'][that.billIndex] += eachPersonOwes;
-			});
-		}
-		/**
-		 * Helper method to get the number of present people on this day.
-		 * @return (int) the number of present people.
-		 */
-		Day.prototype.numPeople = function() {
-			return this.people.length;
-		}
-		
 		/**
 		 * Calculate the bill(s) for the people.
 		 * @param (List) bills a list of bills to calculate.
@@ -53,30 +20,30 @@
 		 * @return (List) the original list of people, each of which has his/her "owes" property
 		 * populated with an array containing the amount of money he/she owes for the bills.
 		 */
-		var calculate = function(bills, people) {
+		calculate = function (bills, people) {
 			// Clone the original lists, so they are not modified by mistake. Also, necessary attributes
 			// that are necessary for calculations later on are added in this step.
 			// Note: Underscore clone() is shallow, so it is necessary to loop over each item in the
 			// original lists and clone it.
-			var results = [];
+			var results = [],
+				billsOutput = [];
 			$.each(people, function (i, person) {
 				var tempPerson = _.clone(person);
 				// moment-range contains() is not inclusive on both ends, so some black magic is required here.
-				tempPerson["range"] = moment().range(moment(tempPerson["in"]).subtract("d", 1), moment(tempPerson["out"]).add("d", 1));
-				tempPerson["owes"] = [];
+				tempPerson.range = moment().range(moment(tempPerson["in"]).subtract("d", 1), moment(tempPerson.out).add("d", 1));
+				tempPerson.owes = [];
 				results.push(tempPerson);
 			});
-			var billsOutput = [];
 			$.each(bills, function (i, bill) {
 				billsOutput.push(_.clone(bill));
 			});
 			$.each(billsOutput, function (billIndex, bill) {
-				bill['start'] = bill['start'].format(dateFormat);
-				bill['end'] = bill['end'].format(dateFormat);
+				bill.start = bill.start.format(dateFormat);
+				bill.end = bill.end.format(dateFormat);
 			});
 
 			// Start the calculations!
-			$.each(bills, function(billIndex, bill) {
+			$.each(bills, function (billIndex, bill) {
 				// In order to calculate the amount of money each person owes, it is necessary to
 				// figure out how many days he/she is in the apt during the cycle. Also, it is 
 				// assumed that everyone uses the same amount of utility in 1 day. With that 
@@ -85,14 +52,28 @@
 				// Then, we can simply divide those amounts to the people that are present in each
 				// day to find the amount that everyone owes.
 				// Be careful, all moments are mutable!
-				var	iterationStep = moment().range(bill['start'], moment(bill['start']).add('days', 1)),
-					range = moment().range(bill['start'], bill['end']),
-					uniqueDays = {}, // Data structure that groups the days with similar # of people together
+				var	iterationStep = moment().range(bill.start,
+						moment(bill.start).add('days', 1)),
+					range = moment().range(bill.start, bill.end),
+					// Data structure that groups the days with similar # of
+					// people together.
+					uniqueDays = {},
 					numColumns = 0,
-					personCount, day;
-				
-				$.each(results, function(i, person) {
-					person['owes'][billIndex] = 0;
+					personCount,
+					day,
+					matrixArray, // the matrix in array form
+					currentRow,
+					resultArray,
+					anchorDayNumPeople,
+					uniqueDayNumPeople,
+					arrayIndex1,
+					arrayIndex2,
+					matrix, // the actual matrix object
+					resultVector,
+					solution;
+
+				$.each(results, function (i, person) {
+					person.owes[billIndex] = 0;
 				});
 				// First step: determine how many people are there in each day, and put it in some
 				// data structure. We need to group the days with the same number of people into 
@@ -101,174 +82,176 @@
 				// data structure using the hash "keys". This is because it's hard to traverse/count
 				// the attributes in Javascript, so it's better to just manually keep track of them
 				// when constructing the data structure.
-				range.by(iterationStep, function(currentDay) {
+				range.by(iterationStep, function (currentDay) {
 					personCount = 0;
 					day = new Day(currentDay, billIndex);
-					$.each(results, function(index, person) {
-						if (person["range"].contains(currentDay)) {
-							personCount ++;
+					$.each(results, function (index, person) {
+						if (person.range.contains(currentDay)) {
+							personCount += 1;
 							day.addPerson(person);
-						};
+						}
 					});
-					if (!('keys' in uniqueDays)) {
-						uniqueDays['keys'] = [];
+					if (!(uniqueDays.hasOwnProperty('keys'))) {
+						uniqueDays.keys = [];
 					}
-					if (!(personCount in uniqueDays)) {
+					if (typeof uniqueDays[personCount] === 'undefined') {
 						uniqueDays[personCount] = [];
-						uniqueDays['keys'].push(personCount.toString());
-						numColumns ++;
+						uniqueDays.keys.push(personCount.toString());
+						numColumns += 1;
 					}
 					uniqueDays[personCount].push(day);
 				});
-				
-				// Start building the matrix. 
-				var matrixArray = [],
-					firstRow = [],
-					resultArray = [],
-					days;
-				// The first row shows the relative weights given to each unique day.
-				$.each(uniqueDays['keys'], function(index, key) {
-					days = uniqueDays[key];
-					firstRow.push(days.length);
+
+				// Start building the matrix.
+				matrixArray = [];
+				currentRow = [];
+				resultArray = [];
+				// The first row shows the relative weights given to each unique
+				// day.
+				$.each(uniqueDays.keys, function (index, key) {
+					currentRow.push(uniqueDays[key].length);
 				});
-				matrixArray.push(firstRow);
-				
+				matrixArray.push(currentRow);
+
 				// Now, we use the first unique category as the anchor, and calculate the
 				// relationship between the rest with respect to the anchor. We then add
 				// this relationship to the necessary row in the matrix.
-				var anchorDayNumPeople = uniqueDays[uniqueDays['keys'][0]][0].numPeople(),
-					uniqueDayNumPeople, currentRow;
-				for (var i=1; i < numColumns; i ++) {
-					uniqueDayNumPeople = uniqueDays[uniqueDays['keys'][i]][0].numPeople();
+				anchorDayNumPeople = uniqueDays[uniqueDays.keys[0]][0].numPeople();
+
+				for (arrayIndex1 = 1; arrayIndex1 < numColumns; arrayIndex1 += 1) {
+					uniqueDayNumPeople = uniqueDays[uniqueDays.keys[arrayIndex1]][0].numPeople();
 					currentRow = [uniqueDayNumPeople];
-					for (var j=1; j < numColumns; j ++) {
-						if (i===j) {
-							currentRow.push(0 - anchorDayNumPeople);
+					for (arrayIndex2 = 1; arrayIndex2 < numColumns; arrayIndex2 += 1) {
+						if (arrayIndex1 === arrayIndex2) {
+							currentRow.push(-anchorDayNumPeople);
 						} else {
 							currentRow.push(0);
 						}
 					}
 					matrixArray.push(currentRow);
 				}
-				
+
 				// The result vector is simple: First value is the total amount of this bill,
 				// and the rest is 0.
-				resultArray.push(bill["total"]);
-				for (var i=0; i < numColumns - 1; i ++) {
+				resultArray.push(bill.total);
+				for (arrayIndex1 = 0; arrayIndex1 < numColumns - 1; arrayIndex1 += 1) {
 					resultArray.push(0);
 				}
-				
+
 				// Making the matrix and the result arrays into actual matrices.
-				var matrix = $M(matrixArray),
-					result = $V(resultArray),
-					solution = matrix.inv().x(result);
-				
+				matrix = $M(matrixArray);
+				resultVector = $V(resultArray);
+				solution = matrix.inv().x(resultVector);
+
 				// The solution matrix has the amount of money that each day is worth for this bill,
 				// so attribute those amounts to the correct days.
-				$.each(solution.elements, function(index, value) {
-					$.each(uniqueDays[uniqueDays['keys'][index]], function(i, day) {
+				$.each(solution.elements, function (index, value) {
+					$.each(uniqueDays[uniqueDays.keys[index]], function (i, day) {
 						day.worths(value);
 					});
 				});
-				
+
 				// In order to to make it easier for output, each person in the results list will
 				// also have a 'total' attribute that is the sum of all the amount they owe in
 				// every bill.
-				$.each(results, function(index, person) {
-					person['total'] = (_.reduce(person['owes'], function(memo, num) { 
-						return memo + num; 
+				$.each(results, function (index, person) {
+					person.total = (_.reduce(person.owes, function (memo, num) {
+						return memo + num;
 					})).toFixed(2);
 				});
 			});
-			
+
 			// In order for easier output, the "owes" attribute for each person is manipulated so that
 			// it contains the whole Bill object as well.
-			$.each(results, function(index, person) {
-				person["owesOld"] = person["owes"];
-				person["owes"] = [];
-				$.each(person["owesOld"], function(billIndex, value) {
-					person["owes"].push({ "bill": billsOutput[billIndex], "amount": value.toFixed(2) });
+			$.each(results, function (index, person) {
+				person.owesOld = person.owes;
+				person.owes = [];
+				$.each(person.owesOld, function (billIndex, value) {
+					person.owes.push({
+						"bill": billsOutput[billIndex],
+						"amount": value.toFixed(2)
+					});
 				});
-				delete person["owesOld"];
+				delete person.owesOld;
 			});
-		
+
 			//console.log(results);
 			return results;
 		};
-		
+
 		/**
 		 * Method to revive the bills from a JSON object.
 		 * @param k key
 		 * @param v value
 		 */
-		var billReviver = function(k, v) {
+		billReviver = function (k, v) {
 			if (v.hasOwnProperty("start") && v.hasOwnProperty("end")) {
 				return billify(v);
 			}
 			return v;
 		};
-		
+
 		/**
 		 * Objectify a naive bill object into one that can be more easily
 		 * processed later on.
 		 * @param (Object) naiveObj the original naive object.
 		 * @return (Object) the finished object.
 		 */
-		var billify = function(naiveObj) {
+		billify = function (naiveObj) {
 			var bill = _.clone(naiveObj);
-			bill["start"] = moment(bill["start"], dateFormat);
-			bill["end"] = moment(bill["end"], dateFormat);
+			bill.start = moment(bill.start, dateFormat);
+			bill.end = moment(bill.end, dateFormat);
 			return bill;
-		}
-		
+		};
+
 		/**
 		 * Helper method to validate a date string.
 		 * @param dateStr the date string to validate.
 		 * @return whether the date string is a valid date.
 		 */
-		var validateDate = function(dateStr) {
+		validateDate = function (dateStr) {
 			return moment(dateStr).isValid();
 		};
-				
+
 		/**
 		 * Method to revive the people from a JSON object.
 		 * @param k key
 		 * @param v value
 		 */
-		var peopleReviver = function(k, v) {
+		peopleReviver = function (k, v) {
 			if (v.hasOwnProperty("name")) {
 				return personify(v);
 			}
 			return v;
 		};
-		
+
 		/**
 		 * Objectify a naive person object so that it can be more easily used
 		 * later on.
 		 * @param (Object) naiveObj the naive person object.
 		 * @return (Object) the finish person object.
 		 */
-		var personify = function(naiveObj) {
+		personify = function (naiveObj) {
 			var person = _.clone(naiveObj);
 			if (!person.hasOwnProperty("in") || !person["in"]) {
 				person["in"] = moment("Jan 01, 1970", dateFormat);
 			} else {
 				person["in"] = moment(person["in"], dateFormat);
 			}
-			if (!person.hasOwnProperty("out") || !person["out"]) {
-				person["out"] = moment("Dec 31, 3000", dateFormat);
+			if (!person.hasOwnProperty("out") || !person.out) {
+				person.out = moment("Dec 31, 3000", dateFormat);
 			} else {
-				person["out"] = moment(person["out"], dateFormat);
+				person.out = moment(person.out, dateFormat);
 			}
 			return person;
 		};
-		
+
 		/**
 		 * Helper Method to dump the people into a JSON object
 		 * @param k key
 		 * @param v value
 		 */
-		var peopleReplacer = function(k, v) {
+		peopleReplacer = function (k, v) {
 			var result = v;
 			if (k === "owes" || k === "range") {
 				return;
@@ -276,24 +259,24 @@
 			if (k === "in" || k === "out") {
 				result = result.format(dateFormat);
 			}
-			if (result === "Dec 31, 3000" || result == "Jan 01, 1970") {
+			if (result === "Dec 31, 3000" || result === "Jan 01, 1970") {
 				return;
 			}
 			return result;
-		}
-		
+		};
+
 		/**
 		 * Helper Method to dump the bills into a JSON object
 		 * @param k key
 		 * @param v value
 		 */
-		var billReplacer = function(k, v) {
+		billReplacer = function (k, v) {
 			if (k === "start" || k === "end") {
 				return v.format(dateFormat);
 			}
 			return v;
 		};
-		
+
 		return {
 			calculate: calculate,
 			billReviver: billReviver,
@@ -302,7 +285,7 @@
 			peopleReplacer: peopleReplacer,
 			billify: billify,
 			personify: personify,
-			validateDate: validateDate,
+			validateDate: validateDate
 		};
 	});
-} ());
+}());
