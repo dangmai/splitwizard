e52272b93fb1c6a658374c20ffe5ec8721cdf9ed
Dang Mai <contact@dangmai.net>
2012-10-08 10:19:58 -0400

    Prompt user to update the website cache

--- www/js/app/main.js
+++ www/js/app/main.js
@@ -1,4 +1,4 @@
-/*global define, moment, console*/
+/*global define, moment, console, Modernizr, window, confirm*/
 (function () {
 	'use strict';
 
@@ -21,6 +21,18 @@
 				// calculate() in order to make it easy to save to cookies
 				people = [],
 				bills = [];
+
+			// Check for new version of the app
+			if (Modernizr.applicationcache) {
+				window.applicationCache.addEventListener('updateready', function (e) {
+					if (window.applicationCache.status === window.applicationCache.UPDATEREADY) {
+						window.applicationCache.swapCache();
+						if (confirm('A new version of this site is available. Load it?')) {
+							window.location.reload();
+						}
+					}
+				}, false);
+			}
 
 			removable = function (count) {
 				return (count !== 1);
