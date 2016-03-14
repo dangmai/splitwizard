8f32470fc7883de86fcddb01a6bb89ff8dfd4d33
Dang Mai <contact@dangmai.net>
2016-03-14 00:50:22 -0400

    Initialize CF CI plugin

--- deploy.sh
+++ deploy.sh
@@ -23,6 +23,7 @@ curl -o cf.tgz -L 'https://cli.run.pivotal.io/stable?release=linux64-binary&vers
 tar xvfz cf.tgz
 ./cf install-plugin -f https://static-ice.ng.bluemix.net/ibm-containers-linux_x64
 # Push to private repo
+./cf ic init
 ./cf login -u $BLUEMIX_USERNAME -p $BLUEMIX_PASSWORD -a api.ng.bluemix.net
 ./cf ic cpi $REPO:latest registry.ng.bluemix.net/$REPO:latest
 OLD_CONTAINER_NAME=`./cf ic ps | grep -oE '[^ ]+$' | grep splitwizard.* | cat`
