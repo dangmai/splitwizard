47a599affc16f3bed300b840b6c397df5acaa059
Dang Mai <contact@dangmai.net>
2016-04-27 18:57:51 -0400

    Creating Production config for webpack

---
+++ webpack.config.prod.js
@@ -0,0 +1,18 @@
+const webpack = require("webpack");
+const devConfig = require("./webpack.config.js");
+
+devConfig.debug = false;
+devConfig.plugins = devConfig.plugins.concat([
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production')
+    }
+  }),
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: true
+    }
+  })
+]);
+
+module.exports = devConfig;
