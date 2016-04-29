f19dcecc497d0286fc73f4f9b2f3827533609a5d
Dang Mai <contact@dangmai.net>
2016-04-28 21:38:35 -0400

    Bring back Apple Touch Icon

--- src/entry.js
+++ src/entry.js
@@ -2,6 +2,7 @@
 
 require("file?name=robots.txt!../static/robots.txt");
 require("file?name=humans.txt!../static/humans.txt");
+require("!!file?name=apple-touch-icon.png!../static/apple-touch-icon.png");
 require("bootstrap/less/bootstrap.less");
 require("./main.css");
 
