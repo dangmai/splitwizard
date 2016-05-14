9df84440d2e17a066040552780955ec371a6eb6b
Dang Mai <contact@dangmai.net>
2016-05-14 17:26:12 -0400

    Update dependencies and fixing resulted bug
    Closes #17, #16, #15, #14, #13, #12

--- src/components/dateInput.js
+++ src/components/dateInput.js
@@ -11,12 +11,25 @@ export default class DateInput extends Component {
 
   render() {
     const { field, ...rest } = this.props;
+
+    const handleChange = (param) => {
+      // We do this so that in the Redux store, the date fields are always
+      // represented as String, so that it's more consistent across browsers.
+      // This fixes an issue in which Firefox and Chrome handle events
+      // differently - Firefox doesn't fire Blue event on the Date Picker
+      // while Chrome does. The bug manifests in that the Redux store would
+      // have Moment objects for the date fields in Firefox, and Strings
+      // for the date fields in Chrome.
+      field.onChange(param ? param.format(dateFormat) : null);
+    };
+
     return (
       <DatePicker
         selected={field.value ? moment(field.value, dateFormat) : null}
         dateFormat={dateFormat}
         {...field}
         {...rest}
+        onChange={handleChange}
       />
     );
   }
